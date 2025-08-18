e72f2b652f37e2801f739947ede09963a64fb10a
pasindupramod755 <pasidubandara12345@gmail.com>
2025-08-18 14:45:54 +0530

    Add ChatObserver

---
+++ src/Demo/ChatObserver.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class ChatObserver {
+
+    private ChatObservable[] chatObservable = new ChatObservable[100];
+    private int nextIndex;
+
+    public void addChatBox(ChatObservable obj) {
+        chatObservable[nextIndex++] = obj;
+    }
+    public void setMessage(String msg){
+        for (int i = 0; i < nextIndex; i++) {
+            chatObservable[i].sendMessage(msg);
+        }
+    }
+}
